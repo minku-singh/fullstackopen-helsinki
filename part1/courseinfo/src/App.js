bf3c3330cc7e3911aa0256338cb119cf39719004
Minku Singh <mailminkusingh@gmail.com>
2022-03-27 18:36:02 +0530

    1.1 - 1.2

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+function Header({course}){
+  return(
+    <h1>{course}</h1>
+  )
+}
+function Part({part, exercises}){
+  return(
+    <p>
+      {part} {exercises}
+    </p>
+  )
+}
+function Content(props){
+  const {part1, exercises1, part2, exercises2, part3, exercises3} = props;
+  return(
+    <>
+      <Part part={part1} exercises = {exercises1} />
+      <Part part={part2} exercises = {exercises2} />
+      <Part part={part3} exercises = {exercises3} />
+    </>
+  )
+}
+function Total(props){
+  const {exercises1, exercises2, exercises3} = props;
+  return(
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  )
+}
+
+
+function App() {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+
+  return (
+    <div>
+      <Header 
+        course={course} 
+      />
+      <Content 
+        part1 = {part1} exercises1 = {exercises1}
+        part2 = {part2} exercises2 = {exercises2}
+        part3 = {part3} exercises3 = {exercises3}
+      />
+      <Total 
+        exercises1 = {exercises1} 
+        exercises2 = {exercises2}
+        exercises3 = {exercises3}
+      />
+    </div>
+  );
+}
+
+export default App;
